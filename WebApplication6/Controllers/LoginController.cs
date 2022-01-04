9493409aa5a86f23ae5eca549edaf9d0ebc48328
Subhan Zulfiqar <66326599+Gi66325452@users.noreply.github.com>
2022-01-04 11:35:45 +0500

    first commit

---
+++ WebApplication6/Controllers/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        SMSEntities db = new SMSEntities();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index(user objchk)
+        {
+
+            if (ModelState.IsValid)
+            {
+                using(SMSEntities db = new SMSEntities())
+                {
+                    var obj = db.users.Where(a => a.username.Equals(objchk.username) && a.password.Equals(objchk.password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.username.ToString();
+                        return RedirectToAction("Index", "registerations");
+                    }
+
+                    else
+                    {
+                        ModelState.AddModelError("", "Username or Password is incorrect");
+                    }
+
+                }
+            }
+
+
+            
+
+
+            return View(objchk);
+        }
+
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+
+    }
+}
